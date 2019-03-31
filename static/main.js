99ac209d588a1bcd3496412cb9fa9c80fdda4066
natlec <accounts@natlec.com>
2019-03-31 21:45:52 +1000

    Improve mobile menu functionality

--- static/main.js
+++ static/main.js
@@ -3,8 +3,53 @@
 		ID: 45423725
 ***/
 
+window.onload = function() {
+    // Enable back button if needed
+    if(document.referrer.indexOf('127.0.0.1:8010') > -1 && history.length > 1) {
+        document.getElementsByClassName('home')[0].classList.add('back');
+    } else {
+        document.getElementsByClassName('home')[0].classList.remove('back');
+    }
+}
+
+// Handle back button click
+document.getElementsByClassName('home')[0].onclick = function() {
+    if(document.referrer.indexOf('127.0.0.1:8010') > -1 && history.length > 1) {
+        window.history.back();
+    }
+};
+
 // Handle menu button click on mobile
 document.getElementsByClassName('menu')[0].onclick = function() {
-    let menu = document.getElementsByTagName('nav')[0];
-    menu.style.display = (menu.style.display != 'block') ? 'block' : 'none';
+    if(window.innerWidth<800) {
+        let menu = document.getElementsByTagName('nav')[0];
+        menu.style.top = (menu.style.top != '80px') ? '80px' : '50px';
+        menu.style.height = (menu.style.height != '270px') ? '270px' : 0;
+        menu.style.padding = (menu.style.padding != '10px 20px 20px') ? '10px 20px 20px' : '0 20px 0';
+    }
 };
+
+// Handle menu hide for unfocus tap on mobile
+document.getElementsByTagName('main')[0].onclick = function() {
+    if(window.innerWidth<800) {
+        let menu = document.getElementsByTagName('nav')[0];
+        menu.style.top = '50px';
+        menu.style.height = 0;
+        menu.style.padding = '0 20px 0';
+    }
+};
+
+// Handle menu hide for resize on mobile
+window.onresize = function() {
+    // Enable back button if needed
+    let menu = document.getElementsByTagName('nav')[0];
+    if(window.innerWidth<800) {
+        menu.style.top = '50px';
+        menu.style.height = 0;
+        menu.style.padding = '0 20px 0';
+    } else {
+        menu.style.top = '';
+        menu.style.height = '';
+        menu.style.padding = '';
+    }
+}
