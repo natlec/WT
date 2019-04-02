fcf6edb079adf82a29d5304fa9a220e89175812b
natlec <accounts@natlec.com>
2019-04-02 15:21:58 +1000

    Minor cosmetic changes

--- static/main.js
+++ static/main.js
@@ -23,7 +23,6 @@ document.getElementsByClassName('home-button')[0].onclick = function() {
 document.getElementsByClassName('menu-button')[0].onclick = function() {
     if(window.innerWidth<800) {
         let menu = document.getElementsByTagName('nav')[0];
-        menu.style.top = (menu.style.top != '80px') ? '80px' : '50px';
         menu.style.height = (menu.style.height != '270px') ? '270px' : 0;
         menu.style.padding = (menu.style.padding != '10px 20px 20px') ? '10px 20px 20px' : '0 20px 0';
     }
@@ -33,7 +32,6 @@ document.getElementsByClassName('menu-button')[0].onclick = function() {
 document.getElementsByTagName('main-button')[0].onclick = function() {
     if(window.innerWidth<800) {
         let menu = document.getElementsByTagName('nav')[0];
-        menu.style.top = '50px';
         menu.style.height = 0;
         menu.style.padding = '0 20px 0';
     }
@@ -44,11 +42,9 @@ window.onresize = function() {
     // Enable back button if needed
     let menu = document.getElementsByTagName('nav')[0];
     if(window.innerWidth<800) {
-        menu.style.top = '50px';
         menu.style.height = 0;
         menu.style.padding = '0 20px 0';
     } else {
-        menu.style.top = '';
         menu.style.height = '';
         menu.style.padding = '';
     }
