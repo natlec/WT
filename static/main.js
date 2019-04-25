29150cc6380128e107f23fb3ed3f2814436ec17c
natlec <accounts@natlec.com>
2019-04-25 16:33:38 +1000

    Fix mobile menu bug

--- static/main.js
+++ static/main.js
@@ -21,15 +21,13 @@ function hideMenu() {
 // Handle menu hide/reset for window resize
 window.onresize = function() { hideMenu() };
 
-if(window.innerWidth < 800) {
-    // Handle menu button click
-    document.querySelector('.menu-button').onclick = function() {
-        menu.style.height = (menu.style.height != '270px') ? '270px' : 0;
-        menu.style.padding = (menu.style.padding != '20px') ? '20px' : '0 20px 0';
-    };
+// Handle menu button click
+document.querySelector('.menu-button').onclick = function() {
+    menu.style.height = (menu.style.height != '270px') ? '270px' : 0;
+    menu.style.padding = (menu.style.padding != '20px') ? '20px' : '0 20px 0';
+};
 
-    // Handle mobile menu hide on scroll or page tap
-    document.querySelector('main').onclick = function() { hideMenu() };
-    window.onscroll = function() { hideMenu() };
-    document.body.ontouchmove = function() { hideMenu() };
-}
+// Handle mobile menu hide on scroll or page tap
+document.querySelector('main').onclick = function() { hideMenu() };
+window.onscroll = function() { hideMenu() };
+document.body.ontouchmove = function() { hideMenu() };
